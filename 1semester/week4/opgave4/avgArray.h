3c00e4bf1eedd2ae1bcfec9e47a6dddb9bc0c5e2
root <root@DESKTOP-RD56JUJ.localdomain>
2022-09-23 18:01:53 +0200

    Update

---
+++ 1semester/week4/opgave4/avgArray.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+
+int avgArray(int *a, int n){
+
+	double res = a[0];
+
+	n += 1;
+
+	for(int i = 1; i < n; i++){
+		res += a[i];
+		printf("%f\n", res);
+	}
+
+	res /= n;
+
+	printf("%f is the average number of the arrayy\n", res);
+
+	return res;
+}
--- 1semester/week4/opgave4/main.c
+++ 1semester/week4/opgave4/main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <assert.h>
+#include "avgArray.h"
 
 int main(){
 
+	int i = 0;
 	int n;
+	double res;
 
-
-	printf("Input the desired length of your array:\n");
+	printf("\nInput the desired length of your array:\n");
 	scanf("%d", &n);
 
 	int a[n];
 
-	for(int i = 0; i < n; i++){
-		scanf("%ld\n", &a[i]);
+	printf("Please input some numbers to find the arvage\n");
+	for(i = 0; i < n; i++){
+		scanf("%d\n", &a[i]);
 	}
+
+
+	for(; i < n; i++){
+		printf("%d\n", a[i]);
+	}
+
+
+	//avgArray(a, n);
+
+	return 0;
 
 }
